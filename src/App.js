8c5dcff2ff8b3e7b27e0963a4c2563a2b3e6c5df
Leah Mattern <ladyleahmattern@gmail.com>
2022-01-02 14:14:10 -0500

    Setting up Browser Router and page components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import { Route, Switch } from 'react-router-dom';
+
+// Page Imports
+import Broken from './pages/404/404';
+import Landing from './pages/landing/Landing';
+import Home from './pages/home/Home';
+import SignUp from './pages/sign-up/SignUp';
+import UserAccount from './pages/user-account/UserAccount';
 
 function App() {
   return (
     <div className="App">
-      
+      <Switch>
+        <Route exact path="/">
+          component={Landing}
+        </Route>
+        <Route path="/404">
+          component={Broken}
+        </Route>
+        <Route path="/home">
+          component={Home}
+        </Route>
+        <Route path="/sign-up">
+          component={SignUp}
+        </Route>
+        <Route path="/account">
+          component={UserAccount}
+        </Route>
+      </Switch>
     </div>
   );
 }
